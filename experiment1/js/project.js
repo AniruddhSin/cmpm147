68e4fe255ed0b344998f9994599ccac4ecd80e2e
AniSindhu <aniruddhsindhu@gmail.com>
2024-04-07 18:50:47 -0700

    uncommented main

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,56 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {  
+    greeting: ["Whats good", "Sup", "Waddap", "Hello", "Hey there", "Yoohoo", "Yerr", "Heya", "Praise the Lord", "Bismallah", "Yo yo yo", "Suh", "Suh dude", "My armpits STINK... anyways how you doing", "Hail Satan", "Shut up and listen to me"],
+    prefix: ["Sir", "Maam", "Bro", "Broseph", "Proffessor", "Gurl", "Gorl", "Girlie", "Madam", "Lord", "Broskie", "Queen", "Your Excellency", "Honorable"],
+    title: ["Rizzler", "Himothy", "Short King", "Bummy McBumface", "Charlatan", "Gringo", "Dank Memer"],
+    role: ["pickup artist", "farmer", "cult overthrower", "demon lord", "princess", "prince", "king", "scam artist", "dropshipper", "salesperson", "hunter", "predator", "captain", "CEO", "test subject"],
+    company: ["Alpha Male Cultivators", "OopsiePoopsie Enterprises", "QuirkyQuark Co.", "OopsieDaisy Enterprises", "CringeMaster Co.", "Yikes Corps", "Randomonium Inc.", "Gen Alpha Literacies", "MemeMachine Innovations", "HappyGoLucky Co.", "TickleTech Solutions"],
+    advertisment: ["zoo", "back of a bottlecap", "3rd page of a newspaper", "reddit 50/50 page", "top of a short bald guy's head", "drunk cosplay convention", "orphanage", "playground", "bottom of a receipt from Walmart", "AA meetup"],
+    emotion: ["Anhedonia", "Euphobia", "Malu", "Eleutheromania", "Cataplexy", "Onism", "Kenopsia", "Exulansis", "Occhiolism", "Monachopsis", "Acedia", "Sonder", "Lachesism", "Liberosis", "Vemödalen"],
+    descriptive: ["Ruthless", "Exploitative", "Monopolistic", "Cutthroat", "Unscrupulous", "Greedy", "Arrogant", "Manipulative", "Opportunistic", "Predatory", "Unethical", "Corrupt", "Overbearing", "Domineering", "Ruthless", "Deceptive", "Unsympathetic", "Mercenary", "Callous", "Machiavellian"],
+    startHour: ["1","2", "3", "4", "5", "6", "7", "8", "9"],
+    endHour: ["4", "5", "6", "7", "8", "9", "10", "11"],
+    wage: ["4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+    adjective: ["Incompetent", "Lazy", "Dishonest", "Unreliable", "Arrogant", "Rude", "Disorganized", "Insubordinate", "Unmotivated", "Irresponsible", "Clueless", "Obnoxious", "Unqualified", "Careless", "Indecisive", "Passive", "Entitled", "Forgetful", "Unprofessional", "Inattentive"]
+  };
+  
+  
+  const template = `$gretting $prefix $title, I want to apply for the role of $role at $company.
+  
+  I saw your flier at the $advertisment and I felt $emotion at the thought of working at your $descriptive company.
+  
+  I agree with the hours of $startHour am to $endHour pm with a wage of $wage dollars per hour and I think that I will be a $adjective prospect for this role.`;
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
